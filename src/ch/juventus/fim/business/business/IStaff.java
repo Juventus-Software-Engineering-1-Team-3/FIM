0f384346a634ec2bb1c87274b639b453900ef309
Marco Kunz <marco@fivenine.ch>
2021-05-26 19:59:48 +0200

    added IStaff interface, #1

---
+++ src/ch/juventus/fim/business/business/IStaff.java
@@ -0,0 +1,16 @@
+package ch.juventus.fim.business.business;
+
+public interface IStaff {
+
+	public int getStaffId();
+	// TODO: implement StaffType
+	//public StaffType getStaffType();
+	//public void setStaffType(StaffType staffType);
+	public String getName();
+	public void setName(String name);
+	public String getSurname();
+	public void setSurname(String surname);
+	public String getPhone();
+	public void setPhone(String phone);
+	
+}
