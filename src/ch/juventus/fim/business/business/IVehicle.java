a5f316e10798903776a74ce1ccb51cec95354794
Marco Kunz <marco@fivenine.ch>
2021-05-26 19:05:07 +0200

    initial Java project commit with IVehicle interface, #1

---
+++ src/ch/juventus/fim/business/business/IVehicle.java
@@ -0,0 +1,22 @@
+package ch.juventus.fim.business.business;
+
+import java.util.Date;
+
+public interface IVehicle {
+	public int getVehicleId();
+	public String getLicensePlate();
+	public void setLicensePLate(String licensePlate);
+	public Date getFirstRegistration();
+	public float getOdometer();
+	public void setOdometer(float odometer);
+	public Date getService();
+	public void setService(Date serice);
+	public Date getMajorService();
+	public void setMajorService(Date majoService);
+	public Date getTires();
+	public void setTires(Date tires);
+	// TODO: Implement ILogEntry and IVehicleType
+	//public ILogEntry[] getLogEntries();
+	//public void addLogEntry(ILogEntry logEntry);
+	//public IVehicleType getVehicleType();
+}
