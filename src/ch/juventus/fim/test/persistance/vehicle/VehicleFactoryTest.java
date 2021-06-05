3f15303a0bbacd77bf29e83350914eafe4355d5d
Marco Kunz <marco@fivenine.ch>
2021-06-05 14:12:53 +0200

    extend vehicleFactory test wirh staff and logEntry, #1

--- src/ch/juventus/fim/test/persistance/vehicle/VehicleFactoryTest.java
+++ src/ch/juventus/fim/test/persistance/vehicle/VehicleFactoryTest.java
@@ -1,10 +1,22 @@
 package ch.juventus.fim.test.persistance.vehicle;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ch.juventus.fim.business.logentry.ILogEntry;
+import ch.juventus.fim.business.logentry.LogEntryFactory;
+import ch.juventus.fim.business.logentry.OilLevel;
+import ch.juventus.fim.business.logentry.TirePressure;
+import ch.juventus.fim.business.staff.IStaff;
+import ch.juventus.fim.business.staff.StaffFactory;
+import ch.juventus.fim.business.staff.StaffType;
 import ch.juventus.fim.business.vehicle.Brand;
 import ch.juventus.fim.business.vehicle.IVehicle;
 import ch.juventus.fim.business.vehicle.IVehicleType;
@@ -26,12 +38,23 @@ public class VehicleFactoryTest {
 	@Test
 	public void createSaveReadVehicleTest() {
 
+		StaffFactory staffFactory = StaffFactory.getinstance();
+		IStaff staff = staffFactory.createStaff(1, StaffType.Driver, "Max", "Muster", "+41 79 123 45 67");
+		staffFactory.saveStaff(staff);
+
+		LogEntryFactory logEntryFactory = LogEntryFactory.getInstance();
+		ILogEntry logEntry = logEntryFactory.createLogEntry(1, "Kommentar", staff, LocalDateTime.now(), OilLevel.Good,
+				TirePressure.Good);
+		logEntryFactory.saveLogEntry(logEntry);
+
 		IVehicleType vehicleType = new VehicleType(Brand.Mercedes, "ModelX");
 
 		// Create vehicles
 		IVehicle bus1 = vehicleFactory.createVehicle(1, "ZH 1", vehicleType);
 		IVehicle bus2 = vehicleFactory.createVehicle(2, "ZH 2", vehicleType);
 		IVehicle bus3 = vehicleFactory.createVehicle(3, "ZH 3", vehicleType);
+
+		bus1.addLogEntry(logEntry);
 
 		// Save vehicles
 		vehicleFactory.saveVehicle(bus1);
@@ -40,9 +63,20 @@ public class VehicleFactoryTest {
 
 		assertEquals(bus1.getLicensePlate(), vehicleFactory.findVehicle(1).getLicensePlate(),
 				"They have not the save license plate!");
+		
+		assertNotNull(bus1.getLogEntries());
+		assertFalse(bus1.getLogEntries().isEmpty());
+
+		assertEquals(bus1.getLogEntries().get(0).getRemarks(), logEntry.getRemarks());
+		assertEquals(bus1.getLogEntries().get(0).getStaff().getStaffType().name(), logEntry.getStaff().getStaffType().name());
+		
+		assertTrue(bus1.getLogEntries().get(0).getTimestamp().isEqual(logEntry.getTimestamp()));
+		
+		
 		assertEquals(bus2.getVehicleType().getBrand().name(),
 				vehicleFactory.findVehicle(2).getVehicleType().getBrand().name(), "They are not fom the same brand!");
-		assertEquals(bus3.getVehicleId(),
-				vehicleFactory.findVehicle(3).getVehicleId(), "They have not the same ID!");
+		
+		assertEquals(bus3.getVehicleId(), vehicleFactory.findVehicle(3).getVehicleId(), "They have not the same ID!");
+
 	}
 }
